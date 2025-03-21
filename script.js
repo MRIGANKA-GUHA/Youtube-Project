a3493c40cb00aa6b9586c0840ff1f14a42581c5c
MRIGANKA-GUHA <mrigankaguha42@gmail.com>
2025-03-22 03:23:00 +0530

    Final commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("video1").onclick = () => window.location.href=("https://www.youtube.com/watch?v=ebtBVb0Vrcs");
+document.getElementById("video2").onclick = () => window.location.href=("https://www.youtube.com/watch?v=QmfyWw3Cth8&t=2s");
+document.getElementById("video3").onclick = () => window.location.href=("https://www.youtube.com/watch?v=W5LPcpIRLzs&t=12s");
+document.getElementById("video4").onclick = () => window.location.href=("https://www.youtube.com/watch?v=YZFt0yXFJBc&t=26s");
+document.getElementById("video5").onclick = () => window.location.href=("https://www.youtube.com/watch?v=JyTylWtvLRA&t=13s");
+document.getElementById("video6").onclick = () => window.location.href=("https://www.youtube.com/watch?v=8xwcT7dHFMI");
+document.getElementById("video7").onclick = () => window.location.href=("https://www.youtube.com/watch?v=S347RCT9LCA&t=8s");
+document.getElementById("video8").onclick = () => window.location.href=("https://www.youtube.com/watch?v=_EKeIyGa-Bg");
+document.getElementById("video9").onclick = () => window.location.href=("https://www.youtube.com/watch?v=oDfK3RfSlKk");
+document.getElementById("video10").onclick = () => window.location.href=("https://www.youtube.com/watch?v=rwxRoYzwkyM");
+document.getElementById("video11").onclick = () => window.location.href=("https://www.youtube.com/watch?v=6cOy5xn8e6E");
+document.getElementById("video12").onclick = () => window.location.href=("https://www.youtube.com/watch?v=2TzgQ5VNhvY");
+document.getElementById("video13").onclick = () => window.location.href=("https://www.youtube.com/watch?v=h1_Dbj696b4");
+document.getElementById("video14").onclick = () => window.location.href=("https://www.youtube.com/watch?v=NKCKtufeg68");
+document.getElementById("video15").onclick = () => window.location.href=("https://www.youtube.com/watch?v=UC63-kScXwI");
+document.getElementById("video16").onclick = () => window.location.href=("https://www.youtube.com/watch?v=tLLFQ1arpYQ&t=10s");
